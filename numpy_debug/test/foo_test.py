b98218cf32d1b0640553cb5fdf5a2a10ca81480d
Felix Duvallet <felix.duvallet@epfl.ch>
2015-12-03 14:15:21 +0100

    Create package, run tests.

---
+++ numpy_debug/test/foo_test.py
@@ -0,0 +1,10 @@
+import unittest
+import numpy as np
+
+class TestCase(unittest.TestCase):
+
+    def test_one(self):
+        self.assertEqual(True, True)
+
+    # def test_two(self):
+    #     self.assertEqual(True, False)
